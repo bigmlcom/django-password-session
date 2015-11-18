54a8cdefc7040c58e1a51f9d0727290e21cee014
Oscr Rovira <rovira@bigml.com>
2015-11-18 12:07:49 +0100

    Avoid logout users with active session that don't have the new key

--- password_session/middleware.py
+++ password_session/middleware.py
@@ -1,11 +1,19 @@
 from django.contrib.auth import logout
 
-from .handlers import get_password_hash, PASSWORD_HASH_KEY
+from .handlers import (
+        get_password_hash,
+        PASSWORD_HASH_KEY,
+        update_session_auth_hash)
 
 
 class CheckPasswordHash(object):
-    """Logout user if value of hash key in session is not equal to current password hash"""
+    """Logout user if value of hash key in session is not equal to current password hash
+       If current user session has no the `PASSWORD_HASH_KEY` update it so we don't logout
+       currently active users.
+    """
     def process_view(self, request, *args, **kwargs):
         if getattr(request.user, 'is_authenticated') and request.user.is_authenticated():
+            if request.session.get(PASSWORD_HASH_KEY) is None:
+                update_session_auth_hash(request, request.user)
             if request.session.get(PASSWORD_HASH_KEY) != get_password_hash(request.user):
                 logout(request)
